f22aee9a1d5a26edbbe3fcc9a42bd2f5fa753381
kenya6565 <ken.australia0605@gmail.com>
2022-11-13 22:06:40 +0900

    create BarPlot.tsx

---
+++ src/components/BarPlot.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Bar, HorizontalBar } from "react-chartjs-2";
+
+const data = {
+  labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+  datasets: [
+    {
+      label: "Demo bar plot",
+      backgroundColor: "#20b2aa",
+      borderColor: "transparent",
+      hoverBackgroundColor: "#5f9ea0",
+      data: [50, 30, 40, 14, 50, 30, 90],
+    },
+  ],
+};
+const BarPlot = () => {
+  return (
+    <div>
+      <Bar data={data} />
+      <HorizontalBar data={data} />
+    </div>
+  );
+};
+
+export default BarPlot;
